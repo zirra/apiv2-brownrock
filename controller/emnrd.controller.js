bf1c2b16348142036e9ce12d65fa3906301f19dd
zirra <msullivan@heysulli.com>
2025-09-24 17:13:07 -0500

    public server test

--- controller/emnrd.controller.js
+++ controller/emnrd.controller.js
@@ -1412,7 +1412,7 @@ class EmnrdController {
 
   // Cron job initialization
   initializeCronJob() {
-      cron.schedule('49 21 * * *', async () => {
+      cron.schedule('14 22 * * *', async () => {
       //cron.schedule('59 23 * * 2', async () => {
       this.filesToProcess = []
       try {
