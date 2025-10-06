5e4bb7a1e82ddcf274052938d3b02ccbc1fb8146
zirra <msullivan@heysulli.com>
2025-10-06 13:45:19 -0500

    test

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const xPolicy = (req, res, next) => {
   res.header ('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS')
   res.header ('Access-Control-Allow-Credentials', 'true')
   res.header ('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, token, X-CSRF-TOKEN, api-key, authorization, content-type, Bearer, ukey')
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
   next ()
 }
 
